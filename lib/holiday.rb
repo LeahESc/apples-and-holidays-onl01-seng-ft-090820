7fe300b7926d6213206ec536c9d6b4eb68a3371d
Leah Schlackman <leahemily1@gmail.com>
2020-09-14 20:49:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays_and_supplies|
-    put "#{season.capiltalize}:"
+    put "#{:season.capiltalize}:"
     holidays_and_supplies.each do |holiday_words, supply_words|
     binding.pry
     holiday_words.split("_")
