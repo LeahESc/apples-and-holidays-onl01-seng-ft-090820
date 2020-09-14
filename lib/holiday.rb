389e5dca44c2aa78fac27bb04e3d31985c4aef39
Leah Schlackman <leahemily1@gmail.com>
2020-09-14 22:11:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,8 +72,8 @@ def all_supplies_in_holidays(holiday_hash)
         supply_array.join(" , ")
          puts "  #{holiday_words.to_s.capitalize}: #{supply_array.join(", ")}"
       end
+    binding.pry
     end
-  binding.pry
   end
 end  
   # iterate through holiday_hash and print items such that your readout resembles:
