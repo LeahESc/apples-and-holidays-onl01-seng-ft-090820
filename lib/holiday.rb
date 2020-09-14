c65d581d2583828cc5e6ab347a478af88a0dbf51
Leah Schlackman <leahemily1@gmail.com>
2020-09-14 22:32:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,10 +92,7 @@ def all_holidays_with_bbq(holiday_hash)
         return holiday
       end
     end
-    
-    # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  end
 end
 
 
