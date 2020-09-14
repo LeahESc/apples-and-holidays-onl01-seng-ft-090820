7bb8d43498267f51ab15b069528bd399e37cc851
Leah Schlackman <leahemily1@gmail.com>
2020-09-14 22:08:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,12 +66,12 @@ def all_supplies_in_holidays(holiday_hash)
     holidays_and_supplies.each do |holiday_words, supply_array|
     holiday_words.to_s
       #binding.pry
-    if holiday_words.to_s.include?("_")
-      holiday_words.to_s.delete("_").capitalize
-    else holiday_words.to_s.capitalize
-      supply_array.join(" , ")
-   # binding.pry
-      puts "  #{holiday_words.to_s.capitalize}: #{supply_array.join(", ")}"
+      if holiday_words.to_s.include?("_")
+        holiday_words.to_s.delete("_").capitalize
+      else holiday_words.to_s.capitalize
+        supply_array.join(" , ")
+   binding.pry
+        puts "  #{holiday_words.to_s.capitalize}: #{supply_array.join(", ")}"
     end
   end
   end
