09d441afd83e3fe671da44ea6b02b512dff3a140
Leah Schlackman <leahemily1@gmail.com>
2020-09-14 20:58:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays_and_supplies|
     puts "#{season.capitalize}:"
     holidays_and_supplies.each do |holiday_words, supply_array|
+    supply_array.join(" , ")
     binding.pry
     puts "#{holiday_words.to_s.capitalize}:"
     supply_array.join(" , ")
