c032a770e0170ab539f8111a12f603f390eaa36c
Leah Schlackman <leahemily1@gmail.com>
2020-09-14 22:32:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,6 +90,7 @@ def all_holidays_with_bbq(holiday_hash)
     hol_hashes.each do |holiday, supply_array|
       if supply_array.include?("BBQ")
         return holiday
+      end
     end
     
     # return an array of holiday names (as symbols) where supply lists
